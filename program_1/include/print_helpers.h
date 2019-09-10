7a184f2fec9ff2de1fc4ec3ff7131a3f38266d17
Jack Campbell <jcampbell@uaudio.com>
2019-09-10 17:08:16 -0600

    Starting to break up functions

---
+++ program_1/include/print_helpers.h
@@ -0,0 +1,10 @@
+//
+// Created by Jack Campbell on 2019-09-10.
+//
+
+#ifndef PROJECT_PRINT_HELPERS_H
+#define PROJECT_PRINT_HELPERS_H
+
+void print_numerical_representations(int32_t inRawNum, int32_t inRadix, int32_t inOpSize);
+
+#endif //PROJECT_PRINT_HELPERS_H
