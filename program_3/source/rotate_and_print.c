6fecfeac29cf566e538248bf029a6f0b347c8c10
Troy Davis <troy.davis@colorado.edu>
2019-09-12 23:22:38 -0600

    Updating code for testing 3/4 binary in LSByte
    * changed this to a lookup table instead of macro

--- program_3/source/rotate_and_print.c
+++ program_3/source/rotate_and_print.c
@@ -18,6 +18,9 @@
 #include "rotate_and_print.h"
 #include "print_binary.h"
 
+// Globals
+const uint8_t 3_4_lookup_table[16] = {0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,0};
+
 /**
  * rotate_and_print
  * @brief Given a pointer to a number, rotate that number in place by a specified amount.
@@ -34,5 +37,16 @@ const char* rotate_and_print(uint32_t* value, uint32_t rotateAmt)
     printf("0x%X\n", (uint16_t)*value);
     print_bin(*value, 16);
     printf(" -> 3 of last 4 bits are on?");
-    return lastThreeBitsOn(*value) ? " true\n" : " false\n";
+    return test_3_4_binary(value) ? " true\n" : " false\n";
+}
+
+/**
+ * test_3_4_binrary 
+ * @brief test if 3 of the 4 last bits are turned on
+ * @param value binary number to run bit analysis on
+ * @return int, true or false whether 3 of 4 bits are turned on
+ */
+int test_3_4_binary(uint32_t value)
+{
+	return lookup_table[value & lsbyte_mask];
 }
